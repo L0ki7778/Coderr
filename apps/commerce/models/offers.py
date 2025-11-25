766eeb9c451dcf7c68d20c60d9bf9ebde0ccd032
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-11-25 22:33:47 +0100

    added offer and offer_detail model

--- apps/commerce/models/offers.py
+++ apps/commerce/models/offers.py
@@ -4,6 +4,30 @@ from apps.accounts.models import User
 
 
 class Offers(models.Model):
-    creator_id = models.ForeignKey(User, unique=True, null=False, blank=False)
-    min_price = models.FloatField(default=1.00)
-    max_delivery_time = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, blank=False, null=False)
+    image = models.ImageField(null=True)
+    description = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Details(models.Model):
+    BASIC = "basic"
+    STANDARD = "standard"
+    PREMIUM = "premium"
+
+    OFFER_TYPES = {
+        BASIC: "basic",
+        STANDARD: "standard",
+        PREMIUM: "premium"
+    }
+
+    offer = models.ForeignKey(
+        Offers, on_delete=models.CASCADE, related_name="details")
+    title = models.CharField(max_length=30, blank=False, null=False)
+    revisions = models.IntegerField(blank=False, null=False)
+    delivery_time_in_days = models.IntegerField(blank=False, null=False)
+    price = models.IntegerField(blank=False, null=False)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(choices=OFFER_TYPES, max_length=12, default=BASIC)
